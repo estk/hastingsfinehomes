aec7259d444e6be44f5545b849b2178be82db1cf
evan <esims89@gmail.com>
2011-08-15 11:37:32 -0700

    Header, Footer, nav and titles

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,20 +1,27 @@
 class PagesController < ApplicationController
   def home
+    @title = nil
+    
   end
 
   def projects
+    @title = 'Projects'
   end
 
   def testimonials
+    @title = 'Testamonials'
   end
 
   def history
+    @title = 'History'
   end
 
   def contact
+    @title = 'Contact'
   end
 
   def partners
+    @title = 'Partners'
   end
 
 end
