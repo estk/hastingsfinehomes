159c16a6e7e4c802612331061a33547af94c1ad1
evan <esims89@gmail.com>
2011-08-17 01:03:49 -0700

    flickrawed

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -4,30 +4,28 @@ module FlickrHelper
   def render_flickr_slideshow
     rescue_flickr do
       photos = get_photo_tags('home')
-      render '/flickr/slideshow', :locals => { :photos => photos }
+      render :partial => '/flickr/slideshow', :locals => { :photos => photos }
     end
   end
   
   def render_flickr_projects
     rescue_flickr do
-      photos = get_photo_tags('pilots')
-      render '/flickr/projects', :locals => { :photos => photos }
+      photos = get_photo_tags('pilot')
+      render :partial => '/flickr/projects', :locals => { :photos => photos }
     end
   end
   
   private
     
     def get_photo_tags(*tags)
-      
       FlickRaw.api_key ||= "f21b3b897a9c08dd4f07f00ca4fdc3f4"
       FlickRaw.shared_secret ||= "c3ac43cfeb6841eb"
-      
-      photos = flickr.photos.search(:user_id => FLICKR_ID, :tags => tags)
+      flickr.photos.search(:user_id => FLICKR_ID, :tags => tags)
     end
     
     def rescue_flickr(&blk)
       blk.call
     rescue Exception => error
-      render '/flickr/unavailable', :locals => { :error => error }
+      render :partial => '/flickr/unavailable', :locals => { :error => error }
     end
 end
