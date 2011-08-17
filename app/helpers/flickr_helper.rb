762be1ad117db17379fff01abf3f436b92d9079f
evan <esims89@gmail.com>
2011-08-16 17:49:29 -0700

    added more flickr machinery

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -1,22 +1,33 @@
-module FlickrHelper
-  
+module FlickrHelper 
   FLICKR_ID = "66318963@N07"
   
   def render_flickr_slideshow
+    rescue_flickr do
+      photos = get_photo_tags('home')
+      render '/flickr/slideshow', :locals => { :photos => photos }
+    end
   end
   
   def render_flickr_projects
+    rescue_flickr do
+      photos = get_photo_tags('pilots')
+      render '/flickr/projects', :locals => { :photos => photos }
+    end
   end
   
   private
-    def flickr_auth
-      FlickRaw.api_key ||= "f21b3b897a9c08dd4f07f00ca4fdc3f4"
-      FlickRaw.shared_secret ||= "c3ac43cfeb6841eb"
-    end
     
     def get_photo_tags(*tags)
-      flickr_auth
+      
+      FlickRaw.api_key ||= "f21b3b897a9c08dd4f07f00ca4fdc3f4"
+      FlickRaw.shared_secret ||= "c3ac43cfeb6841eb"
+      
       photos = flickr.photos.search(:user_id => FLICKR_ID, :tags => tags)
     end
-  
+    
+    def rescue_flickr(&blk)
+      blk.call
+    rescue Exception => error
+      render '/flickr/unavailable', :locals => { :error => error }
+    end
 end
