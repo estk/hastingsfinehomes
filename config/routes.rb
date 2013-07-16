a849d43072f7ab00a20107194651bc780ffa9cc7
Evan Simmons <esims89@gmail.com>
2013-07-16 15:57:31 -0700

    Commented other pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Hastingsco::Application.routes.draw do
   
   root :to => 'pages#home' 
   
-  get '/projects', :to =>     'pages#projects' 
-  get '/testimonials', :to => 'pages#testimonials'
-  get '/history', :to =>      'pages#history' 
+  # get '/projects', :to =>     'pages#projects' 
+  # get '/testimonials', :to => 'pages#testimonials'
+  # get '/history', :to =>      'pages#history' 
   get '/contact', :to =>      'pages#contact' 
-  get '/resources', :to =>     'pages#resources' 
+  # get '/resources', :to =>     'pages#resources' 
 
 end
