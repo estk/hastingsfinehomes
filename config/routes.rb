603b1a7ec2c1324069b427861f168837df1df196
Evan Simmons <esims89@gmail.com>
2013-09-11 23:29:10 -0700

    Re-enabled Testimonials and History Pages.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Hastingsco::Application.routes.draw do
   root :to => 'pages#home' 
   
   # get '/projects', :to =>     'pages#projects' 
-  # get '/testimonials', :to => 'pages#testimonials'
-  # get '/history', :to =>      'pages#history' 
+  get '/testimonials', :to => 'pages#testimonials'
+  get '/history', :to =>      'pages#history' 
   get '/contact', :to =>      'pages#contact'
   # get '/resources', :to =>     'pages#resources' 
 
